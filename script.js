4175c02a85e362b08de141e7818f2b102ecde82f
ulri2307 <ulri2307@stud.kea.dk>
2019-09-14 14:53:46 +0200

    alt virker start

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let alleVarer = [];
-let filter = "alle";
+let filterKategori = "alle";
+let filterPrisklasse = "alle";
+const container = document.querySelector(".data");
+const templateContainer = document.querySelector("template");
 
 document.addEventListener("DOMContentLoaded", hentJson);
 
 async function hentJson() {
-    const jsonData = await fetch("https://spreadsheets.google.com/feeds/list/1mMZ6UI2-PXD05ApvpFvcxDUxDIGkskqlYwOtrnWw4FU/od6/public/values?alt=json");
+    let jsonData = await fetch("https://spreadsheets.google.com/feeds/list/1mMZ6UI2-PXD05ApvpFvcxDUxDIGkskqlYwOtrnWw4FU/od6/public/values?alt=json");
     alleVarer = await jsonData.json();
     visVarer();
     addEventListenersToButtons();
 }
 
 function visVarer() {
-    const container = document.querySelector(".data");
-    const templateContainer = document.querySelector("template");
     container.innerHTML = "";
 
-    alleVarer.feed.entry.forEach((vare) => {
-        if (filter == "alle" || filter == vare.gsx$kategori.$t) {
-            let klon = templateContainer.cloneNode(true).content;
+    alleVarer.feed.entry.forEach(vare => {
+        if ((filterKategori == "alle" || filterKategori == vare.gsx$kategori.$t) && (filterPrisklasse == "alle" || filterPrisklasse == vare.gsx$prisklasse.$t)) {
+            const klon = templateContainer.cloneNode(true).content;
             klon.querySelector("img").src = `nexthand_pics/${vare.gsx$billede.$t}.png`;
             klon.querySelector(".navn").textContent = vare.gsx$navn.$t;
             klon.querySelector(".kort").textContent = vare.gsx$kort.$t;
@@ -29,7 +30,6 @@ function visVarer() {
             });
         }
     })
-
 }
 
 
@@ -37,13 +37,35 @@ function addEventListenersToButtons() {
     document.querySelectorAll(".filter").forEach(elm => {
         elm.addEventListener("click", filtrering);
     })
+    document.querySelectorAll(".filter-prisklasse").forEach(elm => {
+        elm.addEventListener("click", filtreringPrisklasse);
+    })
 }
 
 function filtrering() {
-    filter = this.dataset.kategori;
+    document.querySelector(".filter_prisklasse").classList.remove("hide");
+
+    filterKategori = this.dataset.kategori;
+    if (filterKategori == "alle") {
+        filterPrisklasse = "alle";
+        document.querySelectorAll(".filter-prisklasse").forEach(elm => {
+            elm.classList.remove("valgt");
+        });
+    }
     document.querySelectorAll(".filter").forEach(elm => {
         elm.classList.remove("valgt");
-    })
+    });
+    this.classList.add("valgt");
+    visVarer();
+}
+
+
+function filtreringPrisklasse() {
+    filterPrisklasse = this.dataset.prisklasse;
+    let valgtKategori = document.querySelector(".valgt").textContent;
+    document.querySelectorAll(".filter-prisklasse").forEach(elm => {
+        elm.classList.remove("valgt");
+    });
     this.classList.add("valgt");
     visVarer();
 }
